b56a820fbef29ae5d81f2030c4de5d06a420194a
Raraku <37752943+Raraku@users.noreply.github.com>
2019-11-09 21:55:11 +0100

    Add files via upload

---
+++ precache-manifest.51736392242f9cff3f5e109f12ed845d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d37b3a6a4953b05905844ae39f63a797",
+    "url": "/aire-4/index.html"
+  },
+  {
+    "revision": "acf5f5c4751f39eb39c3",
+    "url": "/aire-4/static/css/2.6ebcfcee.chunk.css"
+  },
+  {
+    "revision": "acf5f5c4751f39eb39c3",
+    "url": "/aire-4/static/js/2.691fce72.chunk.js"
+  },
+  {
+    "revision": "efd753a589fcdcdc9b0e",
+    "url": "/aire-4/static/js/main.fdee24b7.chunk.js"
+  },
+  {
+    "revision": "b118a6df6cf82e06b459",
+    "url": "/aire-4/static/js/runtime-main.5a004cd1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.852ee3a62fc6651f2ddd8bc8797cfe45.js"
+  "/aire-4/precache-manifest.51736392242f9cff3f5e109f12ed845d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/aire-4/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
